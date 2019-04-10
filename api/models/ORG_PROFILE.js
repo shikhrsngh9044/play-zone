75ac99a5d84fe61f8c46d29eac1b4b247e3a2143
shikhrsngh9044 <37914855+shikhrsngh9044@users.noreply.github.com>
2019-04-10 14:49:31 +0530

    org_profile

--- api/models/ORG_PROFILE.js
+++ api/models/ORG_PROFILE.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const orgProfileSchema = mongoose.Schema({
@@ -34,21 +33,3 @@ const orgProfileSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("ORG_PROFILE", orgProfileSchema);
-=======
-const mongoose = require('mongoose');
-
-const orgProfileSchema = mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'USER'
-  },
-  organised_matches: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'MATCH'
-  }],
-  
-});
-
-module.exports = mongoose.model('ORG_PROFILE', orgProfileSchema);
->>>>>>> 93313f04b99e5292ddb0c9021041724735e90c1c
